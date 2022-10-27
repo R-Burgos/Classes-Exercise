0ff42565e5590d7fa5823860275c19515b84d78b
Ruben Burgos <code.rburgos@gmail.com>
2022-10-27 13:46:39 -0500

    Exercise one finished

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var myCar = new Car()
+            {
+                Make = "Toyota",
+                Model = "Camry TRD",
+                Year = 2022,
+            };
+
+            Console.WriteLine($"I own a {myCar.Year} {myCar.Make} {myCar.Model}.");
+
+
+
+
+
+
+
         }
     }
 }
